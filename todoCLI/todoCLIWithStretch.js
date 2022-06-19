e259f3b3c86326f957bc5e9386543b7923220b15
wendyyng <ng_yih_wei@hotmail.com>
2022-06-19 21:35:30 +0000

    Stretch: Save File - Add additional comments

--- todoCLI/todoCLIWithStretch.js
+++ todoCLI/todoCLIWithStretch.js
@@ -1,18 +1,19 @@
-//Save File
-// Add the ability to save a file. Create a new menu item. 
+//Stretch: Save File
+
 const { Todo, toDoCli, writeFile } = require('./todoCLI')
 
 class StretchTodo extends Todo {
     constructor() {
         super();
-        //Menu bar with 'Save' option
+        //A new menu bar with 'Save' option
         this.options = "(v) View • ( n ) New • (cX) Complete • (dX) Delete • (s) Save • (q) Quit"
     }
-    //Stretch: Save File
+    //Save: Add the ability to save a file
     save(fileName) {
         //If a filePath argument has been given, use it as a suggested filePath to save the file. 
         //If the user presses Enter without giving a new filePath, use the suggested filePath
-        //Default fileName is "myTodos.json"
+        //Default suggested fileName is "myTodos.json"
+        //*** Please note that there is already an example json file "myTodos.json" in current directory
         if (!fileName) fileName = "myTodos.json"
         writeFile(fileName, JSON.stringify(this.items))
             .then(console.log(`List saved to "${fileName}"`))
