8dd47e67201005b4f2956d72a4441151d3750fb0
Wendy Ng <yng@ucalgary.ca>
2022-07-01 19:41:32 +0000

    Add assign team method: team count

--- super_team_picker/routes/assign.js
+++ super_team_picker/routes/assign.js
@@ -13,3 +13,29 @@ const shuffleArr = arr => {
     }
   return arr
   }
+
+  //Method: Team Count
+const countTeam = (quantity, members) => {
+  let arr = shuffleArr(members)
+  // console.log(arr)
+  let numMembers = Math.floor(arr.length / quantity)
+  // let numMembers = Math.round(cohort.length / quantity) + 1
+  let result = []
+
+  for(let i = 0; i < quantity ; i++){
+    result.push(arr.splice(0,numMembers))
+  }
+  // console.log(arr)
+  if(arr.length > 0){
+    for(let i = 0; i < result.length; i++){
+      if(arr.length > 0){
+        let next = arr.splice(0,1).flat().toString()
+        // console.log(next)
+        result[i].push(next)
+      }else{
+        break;
+      }
+    }
+  }
+  return result
+}
