406ea5ce7262eed7f0fc2b6b0fc0e3dc1406a4e5
Wendy Ng <yng@ucalgary.ca>
2022-07-01 05:14:40 +0000

    Add index.js and home.ejs

---
+++ super_team_picker/index.js
@@ -0,0 +1,47 @@
+//------Require the express library from the express dependency------>
+const express = require('express');
+const app = express();
+
+// Require method-override middleware
+const methodOverride = require('method-override');
+
+//--------------------BODY PARSER and URLENCODED MIDDLEWARE-------------------->
+app.use(express.urlencoded({extended: true}))
+
+//-----------------Method Override Middleware--------------------------
+app.use(methodOverride((req,res) => {
+    if (req.body && req.body._method) {
+        const method = req.body._method;
+        return method
+    }
+}))
+app.use(express.json())
+
+//-----STATIC ASSETS-------->
+const path = require('path')
+
+//Set up a public directory for these files to reside in
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Logging Middleware----->
+const logger = require('morgan');
+const req = require('express/lib/request');
+app.use(logger('dev'));
+
+//---------------------------ROUTERS--------------------------------->
+//Home Page
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+//SET VIEW ENGINE---->
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+//------------------------------SERVER----------------------------
+//---Start listening to the server---->
+const PORT = 3000;
+const DOMAIN = 'localhost'
+app.listen(PORT, DOMAIN, () => {
+    console.log(`Server is listening on http://${DOMAIN}:${PORT}`)
+})
